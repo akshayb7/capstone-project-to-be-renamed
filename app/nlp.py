2b7e793f47d32dfed25a8e759b4880b99a617118
akshayb7 <akshay10.bhardwaj@gmail.com>
2020-09-12 02:24:18 +0530

    Convert NLP prediction code to a class structure

--- app/nlp.py
+++ app/nlp.py
@@ -1,10 +1,13 @@
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
 
-nlp = pipeline(
-    "sentiment-analysis", 
-    model="distilbert-base-uncased-finetuned-sst-2-english",
-    tokenizer="distilbert-base-uncased-finetuned-sst-2-english"
-    )
+class NLP:
+    def __init__(self):
+        self.pipe = pipeline(
+            "sentiment-analysis",
+            model="distilbert-base-uncased-finetuned-sst-2-english",
+            tokenizer="distilbert-base-uncased-finetuned-sst-2-english"
+            )
 
-result = nlp("I hate you")[0]
-print(f"label: {result['label']}, with score: {round(result['score'], 4)}")
+    def predict_sentiment(self, text: str):      
+        result = self.pipe(text)[0]
+        return f"label: {result['label']}, with score: {round(result['score'], 4)}"
